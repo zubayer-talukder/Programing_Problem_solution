d900161231cd8877b679fd2f72bb9490aeffbfe4
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-16 11:53:04 +0600

    1064 - Positives and Average

---
+++ URI - Beecrowd/1064 - Positives and Average.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float arr[6], sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> arr[i];
+
+        if (arr[i] > 0)
+        {
+            count++;
+            sum += arr[i];
+        }
+    }
+    cout << count << " valores positivos" << endl;
+    cout << fixed << setprecision(1) << sum / count << endl;
+
+    return 0;
+}
