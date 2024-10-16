c36642c22183d760744c8dfc2d811de2f143aea9
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-16 10:56:10 +0600

    1007: Diffirence

---
+++ URI - Beecrowd/1007_Diffirence.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int a,b,c,d,diff;
+    cin>>a>>b>>c>>d;
+    diff = (a * b) - (c * d) ;
+    
+    cout<<"DIFERENCA = "<<diff<<endl;
+    return 0;
+}
