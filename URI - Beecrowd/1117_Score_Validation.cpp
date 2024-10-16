3e28cf308e41a92f41effb5228b6c6b7f9110f9f
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-17 03:19:46 +0600

    1117 - Score Validation

--- URI - Beecrowd/1117_Score_Validation.cpp
+++ URI - Beecrowd/1117_Score_Validation.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
  using namespace std;
 int main() {
-float count =0,x,total,temp=0;
- while(cin>>x){
-     if(x>=0&&x<=10){
-         count++;
-         temp+=x;
-     }
-     else {
-         cout<<"nota invalida"<<endl;
-     }
-     if(count==2)break;
+    float count = 0, x, total, temp = 0;
+    while (cin >> x)
+    {
+        if (x >= 0 && x <= 10)
+        {
+            count++;
+            temp += x;
+        }
+        else
+        {
+            cout << "nota invalida" << endl;
+        }
+        if (count == 2)
+            break;
  }
-    cout<<"media = "<<temp/2.00<<endl;
-    return 0;
+ cout << "media = " << temp / 2.00 << endl;
+ return 0;
 }
