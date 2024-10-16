1aee35e85baaf95d70fdf74be52324478b26d005
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-16 11:01:59 +0600

    1011: Sphere

---
+++ URI - Beecrowd/1010_SImple_calculator.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   // product 1 
+   int pro1,unit1;
+   double price1, total1;
+   cin>>pro1>>unit1>>price1;
+   total1=unit1*price1;
+   
+   //product 2
+    int pro2,unit2;
+   double price2,total2;
+   cin>>pro2>>unit2>>price2;
+    total2=unit2*price2;
+    
+    cout<<"VALOR A PAGAR: R$ "<<fixed<<setprecision(2)<<total1+total2<<endl;
+    return 0;
+}
