6bfd6a808defd0982bb019e1af811a59fe34d840
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-16 10:46:57 +0600

    1005: avg 1

---
+++ URI - Beecrowd/1005_avg_1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   double a,b,avg;
+    cin>>a>>b;
+    avg=((a*3.5)+(b*7.5))/(3.5+7.5);
+    
+    cout<<"MEDIA = "<<fixed<<setprecision(5)<<avg<<endl;
+    return 0;
+}
