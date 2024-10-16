2aff7ad4be24e6f698c9f0c32d1a188e571fb417
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-16 11:06:25 +0600

    1013 - The Greatest

---
+++ URI - Beecrowd/1011-Sphere.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    double pi=3.14159,radius;
+    cin>>radius;
+    cout<<"VOLUME = "<<fixed<<setprecision(3)<<(4/3.0)*pi*radius*radius*radius<<endl;
+   
+   return 0;
+}
