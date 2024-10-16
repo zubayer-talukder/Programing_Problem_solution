dce43fc3d8673f66907916e93a59c31803144e16
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-16 11:44:39 +0600

    1049 : Animal

---
+++ URI - Beecrowd/1049 - Animal.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a, b, c;
+    cin >> a >> b >> c;
+    if (a == "vertebrado")
+    {
+        if (b == "ave")
+        {
+            if (c == "carnivoro")
+            {
+                cout << "aguia" << endl;
+            }
+            else
+                cout << "pomba" << endl;
+        }
+        else if (b == "mamifero")
+        {
+            if (c == "onivoro")
+            {
+                cout << "homem" << endl;
+            }
+            else
+                cout << "vaca" << endl;
+        }
+    }
+
+    if (a == "invertebrado")
+    {
+        if (b == "inseto")
+        {
+            if (c == "hematofago")
+            {
+                cout << "pulga" << endl;
+            }
+            else
+                cout << "lagarta" << endl;
+        }
+        else if (b == "anelideo")
+        {
+            if (c == "hematofago")
+            {
+                cout << "sanguessuga" << endl;
+            }
+            else
+                cout << "minhoca" << endl;
+        }
+    }
+
+    return 0;
+}
