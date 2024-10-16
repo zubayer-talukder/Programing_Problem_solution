f11be2121456be79d1354d59b0eadb6fc59acdb1
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-17 02:47:00 +0600

    1115 : Quadrant

---
+++ URI - Beecrowd/1115_Quadrant.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ int x,y;
+ while(cin>>x>>y){
+ if(x==0 || y==0){
+     break;
+ }
+ else{
+     if(x>0 && y>0){
+         cout<<"primeiro"<<endl;
+     }
+     else if(x>0 && y<0)cout<<"quarto"<<endl;
+     else if(x<0 && y>0)cout<<"segundo"<<endl;
+     else cout<<"terceiro"<<endl;
+ }
+ }
+ 
+    return 0;
+}
+// 
