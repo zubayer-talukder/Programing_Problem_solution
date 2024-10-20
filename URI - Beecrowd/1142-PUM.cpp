7db409ff93501e399f5678a98b6b64d11f887052
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-21 00:12:22 +0600

    1142-PUM

---
+++ URI - Beecrowd/1142-PUM.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+int n;
+cin>>n;
+int x=0;
+ int a=1,b=2,c=3;
+for(int j=0;j<n;j++){
+
+    cout<<a<<" "<<b<<" "<<c<<" PUM"<<endl;
+
+a+=4;
+b+=4;
+c+=4;
+}
+    return 0;
+}
