ab39dcb6e56cfdb48800edc6e21986ec41c76558
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-23 09:10:42 +0600

    1098 - Sequence IJ 4

---
+++ URI - Beecrowd/1098-Sequence_4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+ 
+ for(float i=0;i<=2.2;i+=0.2){
+     for(int j=1;j<=3;j+=1){
+   float  J=j+i;
+       cout<<"I="<<i<<" J="<<J<<endl;
+       
+   }
+}
+ 
+    return 0;
+}
