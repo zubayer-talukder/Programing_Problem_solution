fac6f78bab59fb426a061fdf06a1cefe5eaf5b65
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-26 20:10:08 +0600

    1177 - Array Fill II

---
+++ URI - Beecrowd/1177-Array_Fill_2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t, temp, arr[1000];
+    cin >> t;
+    for (int i = 0; i < 1000; i++)
+    {
+
+        arr[i] = i % t;
+
+        cout << "N[" << i << "] = " << arr[i] << endl;
+         
+     
+     
+ }
+ 
+    return 0;
+}
