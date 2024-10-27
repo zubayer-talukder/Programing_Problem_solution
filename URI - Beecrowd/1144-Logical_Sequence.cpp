8f7e35742407ae73275f52a8b219ea15663d1dbb
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-10-27 20:07:59 +0600

    1144 - Logical Sequence

---
+++ URI - Beecrowd/1144-Logical_Sequence.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using  namespace std; 
+int main() {
+ int x;
+ cin>>x;
+ for(int i=1;i<=x;i++){
+     int a=i,b=i*i,c=i*i*i;
+     cout<<a<<" "<<b<<" "<<c<<endl;
+     cout<<a<<" "<<b+1<<" "<<c+1<<endl;
+     
+ }
+   
+ 
+    return 0;
+}
