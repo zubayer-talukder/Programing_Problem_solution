6922adc0e6559667603cd6ce050091e04ed1f3a3
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-11-04 00:08:44 +0600

    1759 - Ho Ho Ho

---
+++ URI - Beecrowd/1759-Ho_Ho_Ho.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    int x;
+    cin>>x;
+    for(int i=1;i<=x;i++){
+      if(i!=x)  cout<<"Ho ";
+      if(i==x)cout<<"Ho!"<<endl;
+    }
+    return 0;
+}
