2ff8df06566292341b116bffd450ceb1c962b9f4
Abdullah Zubayer <abdullahzubayertalukder.mec@gmail.com>
2024-11-04 01:01:29 +0600

    2483 - Merry Christmaaas!

---
+++ URI - Beecrowd/2483-MC.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    int x;
+    cin>>x;
+    cout<<"Feliz nat";
+ for(int i=0;i<x;i++){
+        cout<<'a';
+        
+    }
+    cout<<"l!"<<endl;
+ 
+    return 0;
+}
